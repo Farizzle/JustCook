140ff101f7610b308347b8597c8ae813b21f0cec
Faris <faris.zaman@metricell.com>
2019-01-27 19:27:05 +0000

    feat: Allow Recipe object to hold more product info

--- JustCook/DataModels/Recipes.swift
+++ JustCook/DataModels/Recipes.swift
@@ -21,13 +21,21 @@ struct Recipes {
     var recipeIcon: String!
     var dietryReqs: dietry!
     var rating: Int?
+    var description: String!
+    var calories: String!
+    var cookTime: String!
+    var fridgeDate: String!
     
-    init(name: String, cuisineType: String, recipeIcon: String, dietryReqs: dietry, rating: Int?) {
+    init(name: String, cuisineType: String, recipeIcon: String, dietryReqs: dietry, rating: Int?, description: String, calories: String, cookTime: String, fridgeDate: String) {
         self.name = name
         self.cuisineType = cuisineType
         self.recipeIcon = recipeIcon
         self.dietryReqs = dietryReqs
         self.rating = rating
+        self.description = description
+        self.calories = calories
+        self.cookTime = cookTime
+        self.fridgeDate = fridgeDate
     }
     
     init?(dictionary: [String: Any]?){
@@ -35,12 +43,16 @@ struct Recipes {
             let name = dictionary["recipeTitle"] as? String,
             let cuisineType = dictionary["recipeCuisine"] as? String,
             let recipeImage = dictionary["recipeImage"] as? String,
-            let recipeRating = dictionary["recipeRating"] as? Int
-            else{
-                return nil
+            let recipeRating = dictionary["recipeRating"] as? Int,
+            let description = dictionary["description"] as? String,
+            let calories = dictionary["calories"] as? String,
+            let cookTime = dictionary["cookTime"] as? String,
+            let fridgeDate = dictionary["fridgeDate"] as? String
+        else{
+            return nil
         }
 
-        self.init(name: name, cuisineType: cuisineType, recipeIcon: recipeImage, dietryReqs: dietry.vegan, rating: recipeRating)
+        self.init(name: name, cuisineType: cuisineType, recipeIcon: recipeImage, dietryReqs: dietry.vegan, rating: recipeRating, description: description, calories: calories, cookTime: cookTime, fridgeDate: fridgeDate)
     }
     
     init?() {
