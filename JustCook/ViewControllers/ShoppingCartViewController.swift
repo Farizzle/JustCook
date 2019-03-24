6bd249b13a989ca90ab6d6cbd666206adfc84055
Faris <faris.zaman@metricell.com>
2019-03-24 15:22:15 +0000

    feat: Add placeholder state for VC when basket is empty

--- JustCook/ViewControllers/ShoppingCartViewController.swift
+++ JustCook/ViewControllers/ShoppingCartViewController.swift
@@ -13,6 +13,7 @@ class ShoppingCartViewController: UIViewController, ShoppingCartPrice {
     @IBOutlet var continuePurchaseButton: UIButton!
     @IBOutlet weak var shoppingCartCollectionView: UICollectionView!
     var shoppingCartViewModel: ShoppingCartViewModel!
+    @IBOutlet weak var emptyFridge: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,11 +31,26 @@ class ShoppingCartViewController: UIViewController, ShoppingCartPrice {
         
         // Configure purchase button
         continuePurchaseButton.layer.borderColor = UIColor.white.cgColor
+    
+    }
+    
+    // Handle purchaseButton action
+    @IBAction func purchaseButton(){
+        if (ShoppingCart.recipes.count != 0){
+            
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
     }
     
     // Update button with total price of items in cart
     func calculateCartPrice(priceString: String) {
-        continuePurchaseButton.setTitle(priceString, for: .normal)
+        if (ShoppingCart.recipes.count != 0){
+            emptyFridge.alpha = 0
+            continuePurchaseButton.setTitle(priceString, for: .normal)
+        } else {
+            continuePurchaseButton.setTitle("Stock up!", for: .normal)
+        }
     }
     
  
